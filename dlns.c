24c9422b7093b2cc8a2d6ac4b30ef384d89f7b8a
Baojun Wang <wangbj@gmail.com>
2019-06-18 11:03:17 -0400

    Add missing file

---
+++ dlns.c
@@ -0,0 +1,45 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/syscall.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <pthread.h>
+#include <elf.h>
+#include <assert.h>
+
+static pthread_key_t local_pkey = -1;
+
+static int gettid(void) {
+  return syscall(SYS_gettid);
+}
+
+static void doing_nothing(void* p)
+{
+  fprintf(stderr, "%u called tls destroy\n", gettid());
+}
+
+int local_getpid(void) {
+
+  if (pthread_getspecific(local_pkey) == NULL) {
+    unsigned long* data = malloc(sizeof(unsigned long));
+    assert(data);
+    pthread_setspecific(local_pkey, data);
+    *data = gettid();
+  }
+
+  unsigned long* cached = pthread_getspecific(local_pkey);
+  assert(cached);
+  return *cached;
+}
+
+__attribute__((constructor)) int dlns_init(void) {
+  fprintf(stderr, "dlns_init\n");
+
+  assert(pthread_key_create(&local_pkey, doing_nothing) == 0);  
+
+  return 0;
+}
